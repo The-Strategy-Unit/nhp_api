5a354809f85b21149d747932caab6c981d0c1c32
Tom Jemmett <thomas.jemmett@nhs.net>
2023-04-03 11:49:00 +0100

    uses dotenv for config values

---
+++ config.py
@@ -0,0 +1,18 @@
+"""Configuration values"""
+# pylint: disable=line-too-long
+
+import os
+
+import dotenv
+
+dotenv.load_dotenv()
+
+STORAGE_ENDPOINT = os.environ["STORAGE_ENDPOINT"]
+SUBSCRIPTION_ID = os.environ["SUBSCRIPTION_ID"]
+CONTAINER_IMAGE = os.environ["CONTAINER_IMAGE"]
+AZURE_LOCATION = os.environ["AZURE_LOCATION"]
+SUBNET_NAME = os.environ["SUBNET_NAME"]
+SUBNET_ID = os.environ["SUBNET_ID"]
+REGISTRY_USERNAME = os.environ["REGISTRY_USERNAME"]
+REGISTRY_PASSWORD = os.environ["REGISTRY_PASSWORD"]
+USER_ASSIGNED_IDENTITY = os.environ["USER_ASSIGNED_IDENTITY"]
