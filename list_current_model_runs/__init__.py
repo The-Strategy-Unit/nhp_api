0aaf0c9628e2d8652b706a9f783d125096e20a16
Tom Jemmett <thomas.jemmett@nhs.net>
2023-05-30 21:07:38 +0100

    adds additional api's
    adds an api to query the status of a specific model run and all
    current containers.
    
    optionally, allow containers that have successfully run to automatically
    be deleted (via config)

---
+++ list_current_model_runs/__init__.py
@@ -0,0 +1,47 @@
+"""List all active model runs (containers)
+"""
+import json
+import logging
+
+import azure.functions as func
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.containerinstance import ContainerInstanceManagementClient
+
+import config
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:  # pylint: disable=unused-argument
+    """main api endpoint"""
+    logging.info("listing all active model runs")
+
+    client = ContainerInstanceManagementClient(
+        DefaultAzureCredential(), config.SUBSCRIPTION_ID
+    )
+    resource_group = "nhp_containers"
+
+    containers = {
+        i.name: _get_container_group_instance_state(i.name, client, resource_group)
+        for i in client.container_groups.list_by_resource_group(resource_group)
+    }
+    return func.HttpResponse(json.dumps(containers), mimetype="application/json")
+
+
+def _get_container_group_instance_state(
+    container_group_name: str,
+    client: ContainerInstanceManagementClient,
+    resource_group: str,
+) -> dict:
+    container = (
+        client.container_groups.get(resource_group, container_group_name)
+        .containers[0]
+        .instance_view.current_state
+    )
+
+    if (
+        config.AUTO_DELETE_COMPLETED_CONTAINERS
+        and container.state == "Terminated"
+        and container.detail_status == "Completed"
+    ):
+        client.container_groups.begin_delete(resource_group, container_group_name)
+
+    return container.as_dict()
