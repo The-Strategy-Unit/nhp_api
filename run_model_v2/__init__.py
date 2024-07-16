1c1159c3b039b6d6398204777be2adfd37667bd5
Tom Jemmett <thomas.jemmett@nhs.net>
2024-07-16 07:58:26 +0100

    ensures id is all lower case

--- run_model_v2/__init__.py
+++ run_model_v2/__init__.py
@@ -66,9 +66,9 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
 def _generate_id(params: str, metadata: dict) -> str:
     crc32 = f"{zlib.crc32(params.encode('utf-8')):x}"
-    scenario_sanitized = re.sub("[^a-z0-9]+", "-", metadata["scenario"].lower())
+    scenario_sanitized = re.sub("[^a-z0-9]+", "-", metadata["scenario"])
     # id needs to be of length 1-63, but the last 9 characters are a - and the hash
-    return f"{metadata['dataset']}-{scenario_sanitized}"[0:54] + "-" + crc32
+    return (f"{metadata['dataset']}-{scenario_sanitized}"[0:54] + "-" + crc32).lower()
 
 
 def _upload_params_to_blob(
